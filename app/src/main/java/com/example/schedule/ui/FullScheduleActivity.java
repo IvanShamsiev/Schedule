834f66ad719a684786bf62027f389701c60449a7
Ivan Shamsiev <ivanshamsiev@yandex.ru>
2019-09-21 16:02:09 +0300

    Some changes with week schedule

--- app/src/main/java/com/example/schedule/ui/FullScheduleActivity.java
+++ app/src/main/java/com/example/schedule/ui/FullScheduleActivity.java
@@ -6,8 +6,6 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -18,6 +16,7 @@ import com.example.schedule.model.Lesson;
 import com.example.schedule.model.Schedule;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 
@@ -29,7 +28,9 @@ public class FullScheduleActivity extends AppCompatActivity {
     ImageButton switchBtn;
 
 
-    FrameLayout layoutDay1, layoutDay2, layoutDay3, layoutDay4, layoutDay5, layoutDay6;
+    FrameLayout layoutDay1, layoutDay2,
+            layoutDay3, layoutDay4,
+            layoutDay5, layoutDay6;
 
     FrameLayout[] layoutDays;
 
@@ -41,7 +42,6 @@ public class FullScheduleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_schedule);
 
-
         setTitle("Расписание по неделям");
 
         // Set toolbar
@@ -51,12 +51,11 @@ public class FullScheduleActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         toolbar.setNavigationOnClickListener(view -> onBackPressed());
 
+
         schedule = ScheduleHelper.getInstance();
 
         twWeekType = findViewById(R.id.twWeekType);
         switchBtn = findViewById(R.id.switchBtn);
-
-
 
         layoutDay1 = findViewById(R.id.layoutDay1);
         layoutDay2 = findViewById(R.id.layoutDay2);
@@ -68,102 +67,56 @@ public class FullScheduleActivity extends AppCompatActivity {
         layoutDays = new FrameLayout[]
                 {layoutDay1, layoutDay2, layoutDay3, layoutDay4, layoutDay5, layoutDay6};
 
-
         switchBtn.setOnClickListener(btn -> {if (isEven) setUnevenWeek(); else setEvenWeek();});
 
-
         setUnevenWeek();
-
     }
 
     void setUnevenWeek() {
-        HashMap<Integer, List<Lesson>> unevenWeek = schedule.getUnevenWeek();
-
-        ArrayList<String> strings = new ArrayList<>(12);
-
-
-        for (int i = 2; i <= unevenWeek.size(); i++) {
-            String text = "";
-            for (Lesson l: unevenWeek.get(i)) text += l.getBeginTime() + " - " + l.getShortName() + "\n";
-
-            strings.add(text);
-        }
-
-        for (FrameLayout l: layoutDays) l.removeAllViews();
-
-        for (int i = 0; i < 6; i++) {
-            View view = getLayoutInflater().inflate(R.layout.week_day_item, layoutDays[i], false);
-            ((TextView) view.findViewById(R.id.textWeekDay)).setText(MainActivity.dayOfWeek[i+1]);
-
-            if (strings.get(i).isEmpty()) {
-                ((TextView) view.findViewById(R.id.textDayLessons)).setText("Нет пар");
-                ((TextView) view.findViewById(R.id.textDayLessons)).setGravity(Gravity.CENTER);
-            } else ((TextView) view.findViewById(R.id.textDayLessons)).setText(strings.get(i));
-            layoutDays[i].addView(view);
-        }
-
-        twWeekType.setText("Верхняя неделя");
         isEven = false;
+        twWeekType.setText("Верхняя неделя");
+
+        setLayouts();
     }
 
     void setEvenWeek() {
-        HashMap<Integer, List<Lesson>> evenWeek = schedule.getEvenWeek();
+        isEven = true;
+        twWeekType.setText("Нижняя неделя");
 
-        ArrayList<String> strings = new ArrayList<>(12);
+        setLayouts();
+    }
 
-        for (int i = 2; i <= evenWeek.size(); i++) {
-            String text = "";
-            for (Lesson l: evenWeek.get(i)) text += l.getBeginTime() + " - " + l.getShortName() + "\n";
+    void setLayouts() {
 
-            strings.add(text);
+        HashMap<Integer, List<Lesson>> week = isEven ? schedule.getEvenWeek() : schedule.getUnevenWeek();
+
+        ArrayList<String> days = new ArrayList<>(6);
+
+        for (int i = 2; i <= week.size(); i++) {
+            StringBuilder lessons = new StringBuilder();
+            for (Lesson l: week.get(i))
+                lessons.append(l.getBeginTime()).append(" - ").append(l.getShortName()).append("\n");
+            days.add(lessons.toString());
         }
+
 
         for (FrameLayout l: layoutDays) l.removeAllViews();
 
         for (int i = 0; i < 6; i++) {
-            View view = getLayoutInflater().inflate(R.layout.week_day_item, layoutDays[i], false);
-            ((TextView) view.findViewById(R.id.textWeekDay)).setText(MainActivity.dayOfWeek[i+1]);
+            View day = getLayoutInflater()
+                    .inflate(R.layout.week_day_item, layoutDays[i], true);
+            TextView twDayOfWeek = day.findViewById(R.id.twDayOfWeek);
+            TextView twDayLessons = day.findViewById(R.id.twDayLessons);
 
-            if (strings.get(i).isEmpty()) {
-                ((TextView) view.findViewById(R.id.textDayLessons)).setText("Нет пар");
-                ((TextView) view.findViewById(R.id.textDayLessons)).setGravity(Gravity.CENTER);
-            } else ((TextView) view.findViewById(R.id.textDayLessons)).setText(strings.get(i));
+            twDayOfWeek.setText(MainActivity.dayOfWeek[i+1]);
+            if (MainActivity.currentDate.get(Calendar.DAY_OF_WEEK) == (i+2) &&
+                    ScheduleHelper.isEven(MainActivity.currentDate) == isEven)
+                twDayOfWeek.setTextColor(getResources().getColor(R.color.colorAccent));
 
-            layoutDays[i].addView(view);
-        }
-
-        twWeekType.setText("Нижняя неделя");
-        isEven = true;
-    }
-
-    private class WeekDayAdapter extends BaseAdapter {
-
-        @Override
-        public int getCount() {
-            return 0;
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            return null;
+            if (days.get(i).isEmpty()) {
+                twDayLessons.setText("Нет пар");
+                twDayLessons.setGravity(Gravity.CENTER);
+            } else twDayLessons.setText(days.get(i));
         }
     }
-
-    /*void editGridView() {
-        gridView.setNumColumns(GridView.AUTO_FIT);
-        gridView.setColumnWidth(160);
-        gridView.setHorizontalSpacing(5);
-        gridView.setVerticalSpacing(5);
-        gridView.setStretchMode(GridView.STRETCH_SPACING_UNIFORM);
-    }*/
 }
